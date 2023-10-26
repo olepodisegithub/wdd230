5e086e03bfa1d95b208b8a2bdbc5296935dd7f7a
Oaitse Lepodise <75835339+olepodisegithub@users.noreply.github.com>
2023-10-26 04:05:17 +0200

    lazy load

---
+++ lesson03/scripts/lazyload.js
@@ -0,0 +1,7 @@
+
+const d = new Date();
+let year = d.getFullYear();
+
+document.getElementById('copyright').innerHTML = `©${year}  Oaitse Lepodise,   Botswana  `;
+
+document.getElementById('lastModified').innerHTML = `Last Modification: ${document.lastModified}`
