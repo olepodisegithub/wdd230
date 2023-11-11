cbe2ef52dadbec72fb6293aec1abc003868dbf1e
Oaitse Lepodise <75835339+olepodisegithub@users.noreply.github.com>
2023-11-11 13:41:17 +0200

    JSON Menu

--- scripts/links.js
+++ scripts/links.js
@@ -1,11 +1,37 @@
 const baseURL = "https://olepodisegithub.github.io/wdd230/";
 const linksURL = baseURL + "data/links.json";
 
+const unorderedList = document.getElementById("lessons");
+
 async function getLinks() 
 {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data.lessons);
 }
   
-  getLinks();
+getLinks();
+
+const displayLinks = (lessons) => 
+{
+    lessons.forEach((lesson) => 
+    {
+        let astring = "";
+        let liElement = document.createElement('li');
+        liElement.textContent = `${lesson.lesson}: `;
+        let aElement = document.createElement('a');
+        lesson.links.forEach((link) => 
+        {
+            if (astring === "") 
+            {
+                astring = `<a href="${link.url}" target="_blank">${link.title}</a>`
+            }
+            else
+            {
+                astring = astring + ` | <a href="${link.url}" target="_blank">${link.title}</a>`
+            }
+        });
+        liElement.innerHTML = `${lesson.lesson}: ` + astring
+        unorderedList.appendChild(liElement)
+    });
+}
