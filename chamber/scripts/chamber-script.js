8c89b0f6781b63488d5f23b02aa03aa3361db9fa
Oaitse Lepodise <75835339+olepodisegithub@users.noreply.github.com>
2023-11-18 10:58:11 +0200

    directory changes

--- chamber/scripts/chamber-script.js
+++ chamber/scripts/chamber-script.js
@@ -76,4 +76,82 @@ switchLink.addEventListener("click", () =>
 /* join form scripts */
 
 const jloadtime = document.getElementById("loadtime");
-jloadtime.value = new Date();
+if (jloadtime  !== null)
+{
+    jloadtime.value = new Date();
+}
+
+/* directory form scripts */
+
+const url = 'data/members.json';
+const cards = document.querySelector('#cards');
+
+async function getMemberData(url) 
+{
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.members);
+    displayMembers(data.members); 
+}
+
+getMemberData(url);
+
+const displayMembers = (members) => 
+{
+    members.forEach((member) => 
+    {
+        let card = document.createElement('section');
+        card.id = "directorysection"
+        let portrait = document.createElement('img');
+        portrait.setAttribute('src', "images/" + member.image);
+        portrait.setAttribute('alt', `Picture of ${member.name}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+        portrait.id = "directoryimg"
+
+        let name = document.createElement('h3');
+        name.textContent = `${member.name}`;
+
+        let address = document.createElement('h3');
+        address.textContent = `${member.address}`;
+
+        let contact = document.createElement('h3');
+        contact.textContent = `${member.phoneNumber}`;
+        
+        let aElement = document.createElement('a');
+        aElement.href = member.websiteURL;
+        aElement.target = "_blank";
+        aElement.innerText = member.name;
+        let astring = `<a href="${member.websiteURL}" target="_blank">${member.websiteURL}</a>`;
+
+        let membership = document.createElement('h3');
+        membership.textContent = `${member.membershipLevel}`;
+    
+        card.appendChild(name);
+        card.appendChild(portrait);
+        card.appendChild(address);
+        card.appendChild(contact);
+        card.appendChild(aElement);
+        card.appendChild(membership);
+    
+        cards.appendChild(card);
+    });
+}
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click", () => 
+{
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList)
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
