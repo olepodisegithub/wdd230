5ec925d94620fd1eb531d152fb9e5e06a271b958
Oaitse Lepodise <75835339+olepodisegithub@users.noreply.github.com>
2023-11-28 04:04:42 +0200

    Collaboration Activity - Weather API

---
+++ scripts/weather.js
@@ -0,0 +1,51 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDexc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.749286458430056&lon=6.635633904335802&units=imperial&appid=7191f9e953ba027452d7d840235d453d';
+
+async function apiFetch()
+{
+    try
+    {
+        const response = await fetch(url);
+        if(response.ok)
+        {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }
+        else
+        {
+            throw Error(await response.text());
+        }
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data)
+{
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = capFirstLetter(data.weather[0].description);
+    weatherIcon.setAttribute('src',iconsrc);
+    weatherIcon.setAttribute('alt',desc);
+    captionDexc.textContent = `${desc}`;
+}
+
+function capFirstLetter(sentence)
+{
+    const arr = sentence.split(" ");
+   
+    for (var i = 0; i < arr.length; i++) 
+    {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+
+    return arr.join(" ");
+}
